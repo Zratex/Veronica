cd4170a13b85f434cf1b8a339509a5a861c8174f
Zratey <83060669+Zratex@users.noreply.github.com>
2021-08-11 10:53:35 +0200

    Ajout définition Dépense et exp
    Dépense permet de calculer et gère la dépense d'un utilisateur en entrain son id ainsi que le montant à payer
    Exp est la gestion du système d'xp, tout en prévoyant un lvl up
    Comme pour Véronica.py, j'ai ajouté des commentaires pour une meilleure visiblité

--- Database.py
+++ Database.py
@@ -2,6 +2,7 @@ import sqlite3
 import datetime
 from sqlite3.dbapi2 import register_converter
 import time
+from random import randint
 
 
 con = sqlite3.connect(f'E:\\Véronica\\Veronica.db')
@@ -21,7 +22,7 @@ def creataccount(id):
     if Verif == 0 : 
         today = datetime.datetime.now()
         today=today.strftime("%d/%m/%Y, %H:%M:%S")
-        insertdata = """INSERT INTO user VALUES (?,0,0,0,0,NULL,?);"""
+        insertdata = """INSERT INTO user VALUES (?,1,0,0,0,NULL,?);"""
         cur.execute(insertdata,(id,today))
         con.commit()
         return(1)
@@ -94,3 +95,50 @@ def testdaily(id):
         cur.execute(sql_select_query, (today, id))
         con.commit()
         return(1)
+
+def exp(id):
+    Recup = "SELECT Exp FROM User WHERE ID_Discord={}".format(id)
+    cur.execute(Recup)
+    expérience=cur.fetchone()[0]
+    xp=randint(5,15)
+    xp=xp+expérience
+    sql_select_query = """update User set Exp = ? where ID_Discord = ?"""
+    cur.execute(sql_select_query, (xp, id))
+    con.commit()
+    #permet de récupérer le niveau actuel de la personne
+    Recup = "SELECT Level FROM User WHERE ID_Discord={}".format(id)
+    cur.execute(Recup)
+    lvl = cur.fetchone()[0]
+    #permet de récupérer la valeur de l'xp requise pour lvl up
+    Recup = "SELECT Xp_requise FROM Levels WHERE Niveau={}".format(lvl)
+    cur.execute(Recup)
+    require = cur.fetchone()[0]
+    if xp >= require: #vérifie si l'xp dépasse la valeur nécessaire pour un rank up
+        #reset d'xp pour le rank up
+        sql_select_query = """update User set Exp = ? where ID_Discord = ?"""
+        xp=0
+        cur.execute(sql_select_query, (xp, id))
+        con.commit()
+        #fait augmenter le niveau
+        sql_select_query = """update User set level = ? where ID_Discord = ?"""
+        lvl=lvl+1
+        result=[1,lvl] #1 pour signifier que c'est un rank up, et le lvl est là pour pouvoir retirer l'information du nouveau niveau
+        cur.execute(sql_select_query, (lvl, id))
+        con.commit()
+        return(result) 
+    else:
+        result=[2,0]
+        return(result) #2 pour signifier que ce n'est pas un rank up
+
+def depense(id,cout):
+    Recup = "SELECT Money FROM User WHERE ID_Discord={}".format(id)
+    cur.execute(Recup)
+    RecupMoney = cur.fetchone()[0]
+    if RecupMoney-cout < 0:
+        return(0) #Pour dire que l'achat n'est pas disponible : l'utilisateur n'a pas assez de thunes
+    else:
+        sql_select_query = """update User set Money = ? where ID_Discord = ?"""
+        RecupMoney=RecupMoney-cout
+        cur.execute(sql_select_query, (RecupMoney, id))
+        con.commit()
+        return(1) #Pour dire que l'achat est disponible, et que la différence a été effectué sur le compte
