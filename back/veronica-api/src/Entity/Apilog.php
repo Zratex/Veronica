718208cfb96a08e859d5289b569a3a810e925175
foul0028 <bryan.fouliard@etudiant.univ-reims.fr>
2024-07-12 16:34:49 +0200

    New incoming try of log implementation

--- back/veronica-api/src/Entity/Apilog.php
+++ back/veronica-api/src/Entity/Apilog.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Apilogs
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
     #[ORM\Column]
     private ?int $id = null;
 
