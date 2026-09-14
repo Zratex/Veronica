290cec984024d54e793f5e1672b3fe22cf7cca43
Zratey <Zratey.AzErTy@gmail.com>
2026-09-14 17:55:47 +0200

    Hotfix : changement de signe tamagotchis

--- cogs/tamagotchi/tamagotchi_main.py
+++ cogs/tamagotchi/tamagotchi_main.py
@@ -9,7 +9,7 @@ class tamagotchi_main(commands.Cog):
     @commands.hybrid_command(name="tamagotchis",description="Liste des tamagotchis")
     async def tamagotchis(self,ctx: commands.Context):
         nbTamas = await count_user_tamagotchis(self.bot.pool,ctx.author.id)
-        if nbTamas > 0:
+        if nbTamas == 0:
             await ctx.send("Tamagotchi à créer")
         else:
             await ctx.send("(à développer)")
