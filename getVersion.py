bb5ec4d48030c61609aa84e9388b09b1a4a010d2
Zratey <Zratey.AzErTy@gmail.com>
2026-09-16 09:07:27 +0200

    retrait du try except pour avoir expliciement l'erreur dans la console

--- getVersion.py
+++ getVersion.py
@@ -4,19 +4,11 @@ def get_version() -> str:
     """
     Retourne la dernière version du projet basée sur le dernier Git tag.
     """
-    try:
-        result = subprocess.run(
-            ["git", "describe", "--tags", "--abbrev=0"],
-            capture_output=True,
-            text=True,
-            check=True,
-        )
+    result = subprocess.run(
+        ["git", "describe", "--tags", "--abbrev=0"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
 
-        return result.stdout.strip()
-
-    except subprocess.CalledProcessError as e:
-        print("Erreur Git :", e.stderr)
-        return "unknown"
-    except FileNotFoundError:
-        print("Git n'est pas installé ou n'est pas accessible.")
-        return "unknown"
+    return result.stdout.strip()
