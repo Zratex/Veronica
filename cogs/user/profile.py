6cb7b751c2924fb2e0bbd949402dab82ddcd0f62
Zratey <Zratey.AzErTy@gmail.com>
2026-09-16 10:29:15 +0200

    Légère adaption de l'embed + coquillettes à tester

--- cogs/user/profile.py
+++ cogs/user/profile.py
@@ -23,10 +23,9 @@ class profile(commands.Cog):
         for key, value in result.items():
             if key == "id":
                 embedVar.set_thumbnail(url="{}".format(user.avatar.url))
-                embedVar.add_field(name="Id Discord :",value="`{}`".format(value), inline=True)
-                embedVar.add_field(name="Pseudo du serveur :",value="{}".format(user.global_name), inline=True)
-                embedVar.add_field(name="Pseudo global :",value="{}".format(user.name), inline=True)
-            elif key == "A EDITER":
+                embedVar.add_field(name="Profile de {}".format(user.global_name),value="id : `{}`".format(value), inline=True)
+                embedVar.add_field(name="Pseudo Discord global :",value="{}".format(user.name), inline=True)
+            elif key == "money":
                 embedVar.add_field(name="<:coquillette:802972160364249119>en solde :",value="{}".format(value), inline=False)
             else:
                 embedVar.add_field(name="{}".format(key),value="{}".format(value), inline=False)
